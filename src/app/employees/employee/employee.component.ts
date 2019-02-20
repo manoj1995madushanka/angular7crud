52cc9b5b14d60afaddfd5180a5cd8d7d415ef211
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-02-20 12:50:25 +0530

    firebase data add function

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {EmployeeService} from '../../shared/employee.service';
 import {NgForm} from '@angular/forms';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-employee',
@@ -9,7 +10,7 @@ import {NgForm} from '@angular/forms';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor(private service: EmployeeService) { }
+  constructor(private service: EmployeeService, private firestore: AngularFirestore) { }
 
   ngOnInit() {
     this.resetForm();
@@ -26,6 +27,12 @@ export class EmployeeComponent implements OnInit {
       empCode: '',
       mobile: '',
     };
+  }
+
+  onSubmit(form: NgForm) {
+    let data = form.value;
+    this.firestore.collection('employees').add(data);
+    this.resetForm(form);
   }
 
 }
